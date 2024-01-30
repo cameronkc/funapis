347af746cccc70d3359af86a8466f8a643f94540
cameronkc <cameronleekcmo@gmail.com>
2024-01-29 23:16:37 -0600

    apikey, cors, https card status

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,10 +6,13 @@
 	export let data: PageData;
 
 	type Product = {
-		title: string,
-		description: string,
-		category: string
-	}
+		title: string;
+		description: string;
+		category: string;
+		auth: string | null;
+		https: boolean;
+		cors: string;
+	};
 
 	const searchApis = data.entries.map((entry) => ({
 		...entry,
@@ -25,28 +28,46 @@
 	});
 </script>
 
-<section class="w-full text-sm">
-	<div class="grid justify-center text-center">
-		<div class="md:input-group input-group-divider m-auto hidden max-w-md grid-cols-[auto_1fr_auto]">
-			<input type="search" placeholder="Search..." bind:value={$searchStore.search} />
-		</div>
-
-			<h1 class="pt-2">
-				{'api count: ' + data.entries.length}
-			</h1>
+<div class="grid justify-center text-center">
+	<div class="md:input-group input-group-divider m-auto hidden max-w-md grid-cols-[auto_1fr_auto]">
+		<input type="search" placeholder="Search..." bind:value={$searchStore.search} />
 	</div>
+
+	<h1 class="pt-2 text-2xl">
+		{'api count: ' + data.entries.length}
+	</h1>
+</div>
+<section class="w-full text-sm">
 	<div class="mt-20 text-center">
 		<ul class="grid gap-4 text-center text-center md:grid-cols-2 lg:grid-cols-3">
 			{#each $searchStore.filtered as entry}
-				<a href={entry.link} target="_blank" class="card card-hover inline-block max-w-96">
-					<li>
-						<header class="card-header text-lg">
+				<a href={entry.link} target="_blank" class="card card-hover inline-block">
+					<li class="flex flex-col h-max">
+						<header class="card-header h-fit text-lg">
 							<strong><h1>{entry.title}</h1></strong>
 						</header>
-						<section class="text-wrap p-4 text-slate-500">
+						<main class="h-fit text-wrap pt-4 text-slate-500 h-20">
 							<p>{entry.description}</p>
-						</section>
-						<footer class="card-footer">(footer)</footer>
+						</main>
+						<footer class="card-footer flex justify-between p-4 ">
+							
+								{#if entry.https}
+									<h3>https</h3>
+								{:else}
+									<h3 class="text-[#3de878]">https</h3>
+								{/if}
+								{#if entry.auth === ''}
+									<h3>API KEY</h3>
+								{:else}
+									<h3 class="text-[#3de878]">API KEY</h3>
+								{/if}
+								{#if entry.cors.toLowerCase() === 'yes'}
+									<h3 class="text-[#3de878]">cors</h3>
+								{:else}
+									<h3>cors</h3>
+								{/if}
+						
+						</footer>
 					</li>
 				</a>
 			{/each}
--- src/routes/category/[slug]/+page.svelte
+++ src/routes/category/[slug]/+page.svelte
@@ -8,6 +8,9 @@
 		description: string;
 		category: string;
 		link: string;
+		https: boolean;
+		auth: string | null;
+		cors: string;
 	}
 
 	let entries: Entry[] = data.entries;
@@ -45,17 +48,35 @@
 	<div class="text-center mt-20">
 		<ul class="grid text-center gap-4 text-center md:grid-cols-2 lg:grid-cols-3">
 			{#each filteredEntries as entry}
-				<a href={entry.link} target="_blank" class="card card-hover max-w-96 inline-block">
-					<li>
-						<header class="card-header text-lg">
-							<strong><h1>{entry.title}</h1></strong>
-						</header>
-						<section class="text-wrap text-slate-500 p-4">
-							<p>{entry.description}</p>
-						</section>
-						<footer class="card-footer">(footer)</footer>
-					</li>
-				</a>
+			<a href={entry.link} target="_blank" class="card card-hover inline-block">
+				<li class="flex flex-col h-max">
+					<header class="card-header h-fit text-lg">
+						<strong><h1>{entry.title}</h1></strong>
+					</header>
+					<main class="h-fit text-wrap pt-4 text-slate-500 h-20">
+						<p>{entry.description}</p>
+					</main>
+					<footer class="card-footer flex justify-between p-4 ">
+						
+							{#if entry.https}
+								<h3>https</h3>
+							{:else}
+								<h3 class="text-[#3de878]">https</h3>
+							{/if}
+							{#if entry.auth === ''}
+								<h3>API KEY</h3>
+							{:else}
+								<h3 class="text-[#3de878]">API KEY</h3>
+							{/if}
+							{#if entry.cors.toLowerCase() === 'yes'}
+								<h3 class="text-[#3de878]">cors</h3>
+							{:else}
+								<h3>cors</h3>
+							{/if}
+					
+					</footer>
+				</li>
+			</a>
 			{/each}
 		</ul>
 	</div>
