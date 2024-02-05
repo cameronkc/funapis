d919907a1cde48571721d00acefdb3e747675c5e
cameronkc <cameronleekcmo@gmail.com>
2024-02-04 21:03:44 -0600

    update css for footer in card design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,49 +27,56 @@
 		unsubscribe();
 	});
 </script>
+
 <div class="grid justify-center text-center">
 	<div class="pb-10">
 		<strong>
-			<span class="text-5xl bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent md:text-6xl lg:text:8xl">
+			<span
+				class="lg:text:8xl bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-5xl text-transparent md:text-6xl"
+			>
 				Funapis, a collection of over 1,500 API's.
 			</span>
-	
 		</strong>
 	</div>
 	<div class="input-group input-group-divider m-auto max-w-md grid-cols-[auto_1fr_auto]">
 		<input type="search" placeholder="Search..." bind:value={$searchStore.search} />
 	</div>
 </div>
-<section class="w-full text-sm">
+<section class="w-full">
 	<div class="mt-20 text-center">
-		<ul class="grid gap-4 text-center text-center md:grid-cols-2 lg:grid-cols-3">
+		<ul class="mt-20 grid gap-4 text-center text-center md:grid-cols-2 lg:grid-cols-3">
 			{#each $searchStore.filtered as entry}
-				<a href={entry.link} target="_blank" class="card card-hover inline-block p-4 m-4">
-					<li class="flex flex-col h-max">
-						<header class="card-header h-fit text-lg">
+				<a
+					href={entry.link}
+					target="_blank"
+					class="card card-hover m-4 inline-block min-w-64 lg:p-4"
+				>
+					<li class="relative flex flex-col">
+						<header class="card-header text-lg">
 							<strong><h1>{entry.title}</h1></strong>
 						</header>
-						<main class="h-fit text-wrap pt-4 text-slate-500 h-20">
+						<main class="h-fit text-wrap p-4 pt-20">
 							<p>{entry.description}</p>
 						</main>
-						<footer class="card-footer flex justify-between">
-							
-								{#if entry.https}
-									<h3>https</h3>
-								{:else}
-									<h3 class="text-[#3de878]">https</h3>
-								{/if}
-								{#if entry.auth === ''}
-									<h3>API KEY</h3>
-								{:else}
-									<h3 class="text-[#3de878]">API KEY</h3>
-								{/if}
-								{#if entry.cors.toLowerCase() === 'yes'}
-									<h3 class="text-[#3de878]">cors</h3>
-								{:else}
-									<h3>cors</h3>
-								{/if}
-						
+						<footer
+							class="card-footer absolute top-5 w-max justify-center text-sm text-slate-400 lg:top-0"
+						>
+							{#if entry.https}
+								<h3>https</h3>
+							{:else}
+								<h3 class="text-[#3de878]">https</h3>
+							{/if}
+							{#if entry.auth === ''}
+								<h3>API KEY</h3>
+							{:else}
+								<h3 class="text-[#3de878]">API KEY</h3>
+							{/if}
+
+							{#if entry.cors.toLowerCase() === 'yes'}
+								<h3 class="text-[#3de878]">cors</h3>
+							{:else}
+								<h3>cors</h3>
+							{/if}
 						</footer>
 					</li>
 				</a>
--- src/routes/category/[slug]/+page.svelte
+++ src/routes/category/[slug]/+page.svelte
@@ -39,47 +39,48 @@
 	// ... other script code ...
 </script>
 
-	<h1 class="h1 p-4">
-		<strong>
-			<span class="bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone">{ currentCategoryTitle }</span>
-		</strong>
-	</h1>
-	
-	<div class="text-center mt-20">
-		<ul class="grid text-center gap-4 text-center md:grid-cols-2 lg:grid-cols-3">
-			{#each filteredEntries as entry}
-			<a href={entry.link} target="_blank" class="card  card-hover inline-block lg:p-4 m-4">
-				<li class="flex flex-col h-max">
-					<header class="card-header h-fit text-lg">
+<h1 class="h1 p-4">
+	<strong>
+		<span
+			class="bg-gradient-to-br from-blue-500 to-cyan-300 box-decoration-clone bg-clip-text text-transparent"
+			>{currentCategoryTitle}</span
+		>
+	</strong>
+</h1>
+
+<div class="mt-20 text-center">
+	<ul class="grid gap-4 text-center text-center md:grid-cols-2 lg:grid-cols-3">
+		{#each filteredEntries as entry}
+			<a href={entry.link} target="_blank" class="card card-hover m-4 inline-block min-w-64 lg:p-4">
+				<li class="relative flex flex-col">
+					<header class="card-header text-align text-wrap max-w-44 m-auto text-lg">
 						<strong><h1>{entry.title}</h1></strong>
 					</header>
-					<main class="h-fit text-wrap p-4 text-slate-500 h-20">
+					<main class="h-fit text-wrap p-4 pt-20">
 						<p>{entry.description}</p>
 					</main>
-					<footer class="card-footer flex justify-between p-4 ">
-						
-							{#if entry.https}
-								<h3>https</h3>
-							{:else}
-								<h3 class="text-[#3de878]">https</h3>
-							{/if}
-							{#if entry.auth === ''}
-								<h3>API KEY</h3>
-							{:else}
-								<h3 class="text-[#3de878]">API KEY</h3>
-							{/if}
-							{#if entry.cors.toLowerCase() === 'yes'}
-								<h3 class="text-[#3de878]">cors</h3>
-							{:else}
-								<h3>cors</h3>
-							{/if}
-					
+					<footer
+						class="card-footer absolute top-5 w-max justify-center text-sm text-slate-400 lg:top-0"
+					>
+						{#if entry.https}
+							<h3>https</h3>
+						{:else}
+							<h3 class="text-[#3de878]">https</h3>
+						{/if}
+						{#if entry.auth === ''}
+							<h3>API KEY</h3>
+						{:else}
+							<h3 class="text-[#3de878]">API KEY</h3>
+						{/if}
+
+						{#if entry.cors.toLowerCase() === 'yes'}
+							<h3 class="text-[#3de878]">cors</h3>
+						{:else}
+							<h3>cors</h3>
+						{/if}
 					</footer>
 				</li>
 			</a>
-			{/each}
-		</ul>
-	</div>
-
-	
-
+		{/each}
+	</ul>
+</div>
